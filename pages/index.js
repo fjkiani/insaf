4b3bef3fbe956045a1619e7a81a60330d758e4ff
Fahad Kiani <fahadkiani@fahads-mbp.lan>
2023-02-05 01:42:48 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    {/* {console.log(bannerData)} */}
+    <div className="products-heading">
+      {/* <h2>Recent </h2>
+      <p>speaker There are many variations passages</p> */}
+    </div>
+
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
+
+    {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'; //grab all products from product dashboard 
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]'; //fetch everything where type is equal to banner 
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+}
+
+export default Home;
