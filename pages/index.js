c3866c2c9741b55ff1c4a40ac50e91f567cc5f55
Fahad Kiani <fahadkiani@Fahads-MacBook-Pro.local>
2023-02-05 20:40:12 -0500

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,28 @@
 import React from 'react';
-
 import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    {/* <HeroBanner heroBanner={bannerData.length && bannerData[0]}  /> */}
     {/* {console.log(bannerData)} */}
     <div className="products-heading">
-      {/* <h2>Recent </h2>
-      <p>speaker There are many variations passages</p> */}
     </div>
 
     <div className="products-container">
       {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
-
-    {/* <FooterBanner footerBanner={bannerData && bannerData[0]} /> */}
   </div>
 );
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async () => { //utilze this function when fetching from an API or CMS 
   const query = '*[_type == "product"]'; //grab all products from product dashboard 
   const products = await client.fetch(query);
 
   const bannerQuery = '*[_type == "banner"]'; //fetch everything where type is equal to banner 
   const bannerData = await client.fetch(bannerQuery);
 
-  return {
+  return { 
     props: { products, bannerData }
   }
 }
